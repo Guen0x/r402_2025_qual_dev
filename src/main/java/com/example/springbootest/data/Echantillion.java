b14c76bccd5d96ca7b02960d8c3552dac80d9772
GUENOUNOU Stephane <12301026@q10218.iutv.univ-paris13.fr>
2025-05-05 11:51:47 +0200

    Test implementation sattistique ajout de echantillon

---
+++ src/main/java/com/example/springbootest/data/Echantillion.java
@@ -0,0 +1,35 @@
+package com.example.springbootest.service;
+
+/**
+ * Classe de données qui permet d'encpsulée de données
+ *   * Nombre de voitures
+ *   * Prix moyen de ce nombre de voiture(s)
+ */
+public class Echantillon {
+
+    int nombreDeVoitures;
+    int prixMoyen;
+
+    public Echantillon(){}
+
+    public Echantillon(int nombreDeVoitures, int prixMoyen) {
+        this.nombreDeVoitures = nombreDeVoitures;
+        this.prixMoyen = prixMoyen;
+    }
+
+    public int getNombreDeVoitures() {
+        return nombreDeVoitures;
+    }
+
+    public void setNombreDeVoitures(int nombreDeVoitures) {
+        this.nombreDeVoitures = nombreDeVoitures;
+    }
+
+    public int getPrixMoyen() {
+        return prixMoyen;
+    }
+
+    public void setPrixMoyen(int prixMoyen) {
+        this.prixMoyen = prixMoyen;
+    }
+}
